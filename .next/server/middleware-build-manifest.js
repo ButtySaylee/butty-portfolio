f7142611463cd24a4dc4396ca9a3a4cbc4f92dc0
ButtySaylee <nbs917740@gmail.com>
2026-02-24 15:15:36 +0530

    fixed resume download error

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-71114ce99c3af532.js","static/chunks/fd9d1056-6402681317ea8432.js","static/chunks/117-ba886579cdf9bdc7.js","static/chunks/main-app-322c511d922e523e.js"],pages:{"/_app":["static/chunks/webpack-71114ce99c3af532.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-778e03ab42914a0b.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-71114ce99c3af532.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-778e03ab42914a0b.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
